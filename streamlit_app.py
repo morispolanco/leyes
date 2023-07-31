6366e5d49388b287c4203e52e1cbf773e865b3f0
morispolanco <morispolanco@gmail.com>
2023-07-30 18:42:45 -0600

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import requests
+
+# Set up the Dify AI API endpoint and secret key
+dify_endpoint = "https://api.dify.ai/v1/chat-messages"
+secret_key = "app-OZw6qix4wsjQl4MUTmlpEukZ"
+
+def send_message(text):
+    # Create a JSON payload with the input text and other metadata
+    payload = {
+        "inputs": {"text": text},
+        "query": "eh",
+        "response_mode": "streaming",
+        "conversation_id": "",
+        "user": ""
+    }
+
+    # Set up the HTTP headers with the Authorization token
+    headers = {
+        "Authorization": f"Bearer {secret_key}",
+        "Content-Type": "application/json"
+    }
+
+    try:
+        # Send the request to the Dify AI API
+        response = requests.post(dify_endpoint, json=payload, headers=headers)
+
+        # Print the response from the Dify AI API (if there is one)
+        print(response.json())
+    except Exception as e:
+        # Print an error message if there was an issue sending the request
+        print(f"Error sending message: {e}")
+
+st.title("Dify AI Chat")
+st.write("Enter a message: ")
