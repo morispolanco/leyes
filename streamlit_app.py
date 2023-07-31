5616de8e96bc0f619c252575139ed86be4851d2c
morispolanco <morispolanco@gmail.com>
2023-07-30 19:28:55 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,29 @@
 import streamlit as st
-import requests
+import websocket
+import json
 
 # Set up the Dify AI API endpoint and secret key
 dify_endpoint = "https://api.dify.ai/v1/chat-messages"
 secret_key = "app-OZw6qix4wsjQl4MUTmlpEukZ"
 
 
-def send_message(text, conversation_id):
+# Global variables
+conversation_id = ""
+response_received = False
+
+def on_message(ws, message):
+    global response_received
+    response_data = json.loads(message)
+    if "output" in response_data:
+        output_text = response_data["output"]["text"]
+        st.write("Response:")
+        st.write(output_text)
+        response_received = True
+
+def send_message(text):
+    global conversation_id, response_received
+    response_received = False
+
     # Create a JSON payload with the input text and other metadata
     payload = {
         "inputs": {"text": text},
@@ -16,47 +33,29 @@ def send_message(text, conversation_id):
         "user": ""
     }
 
-    # Set up the HTTP headers with the Authorization token
-    headers = {
-        "Authorization": f"Bearer {secret_key}",
-        "Content-Type": "application/json"
-    }
+    # Set up the WebSocket connection
+    ws = websocket.WebSocketApp(
+        dify_endpoint,
+        on_message=on_message,
+        header={"Authorization": f"Bearer {secret_key}"}
+    )
 
-    try:
-        # Send the request to the Dify AI API
-        response = requests.post(dify_endpoint, json=payload, headers=headers)
+    # Send the payload as a message
+    ws.send(json.dumps(payload))
 
-        # Get the response from the Dify AI API (if there is one)
-        if response.status_code == 200:
-            response_data = response.json()
-            if "output" in response_data:
-                output_text = response_data["output"]["text"]
-                return output_text
-            else:
-                print("No output text found in the response")
-        else:
-            print(f"Request failed with status code: {response.status_code}")
-            print(response.text)
+    # Wait for response
+    while not response_received:
+        ws.run_forever()
 
-    except Exception as e:
-        # Print an error message if there was an issue sending the request
-        print(f"Error sending message: {e}")
+    # Close the WebSocket connection
+    ws.close()
 
 # Streamlit interface
 st.title("Dify AI Chat")
-
-# Initialize conversation ID
-conversation_id = ""
 
 # Get user input
 user_input = st.text_input("Enter a message")
 
 # Send user input as a message
 if st.button("Send"):
-    response = send_message(user_input, conversation_id)
-    if response:
-        st.write("Response:")
-        st.write(response)
-        conversation_id = ""  # Update with the actual conversation ID from the response
-    else:
-        st.write("No response received")
+    send_message(user_input)
