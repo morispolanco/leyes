5ae83dc56da8fcc1f27f3ecec44f7dbc13772753
morispolanco <morispolanco@gmail.com>
2023-07-30 19:46:24 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,53 +1,48 @@
 import streamlit as st
-import socketio
+import requests
 
 # Set up the Dify AI API endpoint and secret key
 dify_endpoint = "https://api.dify.ai/v1/chat-messages"
 secret_key = "app-OZw6qix4wsjQl4MUTmlpEukZ"
 
 
+
 # Global variables
 conversation_id = ""
-response_received = False
-
-def on_message(data):
-    global response_received
-    if "output" in data:
-        output_text = data["output"]["text"]
-        st.write("Response:")
-        st.write(output_text)
-        response_received = True
 
 def send_message(text):
-    global conversation_id, response_received
-    response_received = False
+    global conversation_id
 
-    # Set up the SocketIO connection
-    sio = socketio.Client()
-
-    @sio.on("message")
-    def handle_message(data):
-        on_message(data)
-
-    # Connect to the SocketIO server
-    sio.connect(dify_endpoint, headers={"Authorization": f"Bearer {secret_key}"})
-
-    # Send the message payload
+    # Create the payload with the input text and other metadata
     payload = {
         "inputs": {"text": text},
         "query": "eh",
         "response_mode": "streaming",
-        "conversation_id": conversation_id,
+        "conversation_id": "",
         "user": ""
     }
-    sio.emit("message", payload)
 
-    # Wait for response
-    while not response_received:
-        sio.sleep(0.1)
+    # Set the headers with the secret key
+    headers = {
+        "Authorization": f"Bearer {secret_key}",
+        "Content-Type": "application/json"
+    }
 
-    # Disconnect from the SocketIO server
-    sio.disconnect()
+    # Send the POST request to the Dify AI API
+    response = requests.post(dify_endpoint, json=payload, headers=headers)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        response_data = response.json()
+        if "output" in response_data:
+            output_text = response_data["output"]["text"]
+            st.write("Response:")
+            st.write(output_text)
+
+        # Update the conversation ID for maintaining context
+        conversation_id = response_data.get("conversation_id", "")
+    else:
+        st.write("Error occurred while sending the message")
 
 # Streamlit interface
 st.title("Dify AI Chat")
